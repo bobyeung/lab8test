30a21166bb83bebe85646933ae37938c5d4ce703
onionhaha <yeungchungyin1999@gmail.com>
2019-04-11 11:43:44 +0800

    kim after

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -6,6 +6,7 @@ public class Library {
 	public static void main(String arg[]) {
 		System.out.println("trump!");
 		System.out.println("kim !");
+		System.out.println();
 	}
 	
     public boolean someLibraryMethod() {
